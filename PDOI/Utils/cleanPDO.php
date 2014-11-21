b1fea35bfa7883c76b05ceac5c1b27900a87c1c5
schennault <schenn@mash.is>
2014-11-21 12:27:28 -0800

    Extracted cleanPDO to its own file
    Code cleanup, I moved cleanPDO to its own file to cleanup PDOI file

---
+++ PDOI/Utils/cleanPDO.php
@@ -0,0 +1,87 @@
+<?php
+namespace PDOI\Utils;
+use PDO;
+/**
+ * @author Steven Chennault schenn@gmail.com
+ * @link: https://github.com/Schenn/PDOI Repository
+ */
+
+/**
+ * Class cleanPDO
+ *
+ * Constructs a PDO ready to handle errors using a dictionary of configuration options.
+ *
+ * @package PDOI
+ * @internal
+ */
+
+class cleanPDO extends PDO {
+    /** @var bool $hasActiveTransaction Is the PDO currently in the middle of a transaction */
+    protected $hasActiveTransaction = false;
+
+    /**
+     * Create a new cleanPDO
+     *
+     * @param array $config  The dictionary of configuration options
+     *  'dbname'=>'pdoi_tester',
+     *  'host'=>'127.0.0.1',
+     *  'username'=>'pdoi_tester',
+     *  'password'=>'pdoi_pass',
+     *  'driver_options'=>[PDO::ATTR_PERSISTENT => true]
+     *
+     */
+    function __construct($config){
+        $dns = 'mysql:dbname='.$config['dbname'].';';
+        if(isset($config['host'])){
+            $dns .= $config['host'];
+        } else {
+            $dns .= '127.0.0.1';
+        }
+        if(!isset($config['driver_options'])){
+            $config['driver_options'] = [PDO::ATTR_PERSISTENT => true];
+        }
+        $this->hasActiveTransaction = false;
+        parent::__construct($dns, $config['username'], $config['password'], $config['driver_options']);
+        parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    /**
+     * Start working
+     *
+     * If the pdo is already working, fail, otherwise begin a transaction
+     * @return bool
+     */
+    function beginTransaction() {
+        if($this->hasActiveTransaction){
+            return(false);
+        }
+        else {
+            $this->hasActiveTransaction = parent::beginTransaction();
+            return($this->hasActiveTransaction);
+        }
+    }
+
+    /**
+     * Commit the work
+     *
+     * Commit the work the pdo has done and mark as not working
+     *
+     * @return bool
+     */
+    function commit() {
+        $this->hasActiveTransaction = false;
+        return(parent::commit());
+    }
+
+    /**
+     * Rollback the work
+     *
+     * Rollback the work the pdo has done and mark as not working
+     *
+     * @return bool
+     */
+    function rollback() {
+        $this->hasActiveTransaction = false;
+        return(parent::rollBack());
+    }
+}
