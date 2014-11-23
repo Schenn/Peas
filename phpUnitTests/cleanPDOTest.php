dd4624b3d1648db83954ab7ad6f32a43247b7161
Steven Chennault <schenn@gmail.com>
2014-11-23 15:47:38 -0800

    Have retrieve data get the actual inserted row

--- phpUnitTests/cleanPDOTest.php
+++ phpUnitTests/cleanPDOTest.php
@@ -2,6 +2,11 @@
 include_once("..\PDOI\Utils\cleanPDO.php");
 use PDOI\Utils\cleanPDO as cleanPDO;
 
+/**
+ * Class cleanPDOITest
+ *
+ * Unit Tests for PDOI\Utils\cleanPDO, the 'safe' pdo
+ */
 class cleanPDOITest extends PHPUnit_Framework_TestCase {
     // Disable the persistent nature of cleanPdo due to the unique nature of the test environment.
     // Lots of new pdos in this test and it was leading to a crash in php
@@ -12,11 +17,17 @@ class cleanPDOITest extends PHPUnit_Framework_TestCase {
         'driver_options'=> [PDO::ATTR_PERSISTENT => false]
     ];
 
+    /**
+     * Ensure we can create a new cleanPDO with a good config
+     */
     public function testCreation(){
         $cleanPDO = new cleanPDO($this->goodConfig);
         $this->assertInstanceOf("PDOI\Utils\cleanPDO", $cleanPDO);
     }
 
+    /**
+     * Ensure that attempting to create a new cleanPDO with a bad config throws an Exception
+     */
     public function testCreationFails(){
         try {
             $cleanPDO = new cleanPDO([
@@ -39,15 +50,20 @@ class cleanPDOITest extends PHPUnit_Framework_TestCase {
     }
 
     /**
+     * Ensure the cleanPDO can insert and retrieve the data it inserted into the database
+     *
      * @depends testCreation
      * */
     public function testCanInsertAndRetrieveData(){
         $cleanPDO = new cleanPDO($this->goodConfig);
         $cleanPDO->beginTransaction();
         $cleanPDO->query("INSERT INTO test_table VALUES(null, 1)");
-        $this->assertTrue($cleanPDO->commit());
 
-        $stmt = $cleanPDO->query("SELECT * FROM `test_table` WHERE `value` = 1 LIMIT 1");
+        $id = $cleanPDO->lastInsertId();
+        $this->assertTrue($id != 0);
+        $stmt = $cleanPDO->query("SELECT * FROM `test_table` WHERE `key` = $id");
+
+        $this->assertTrue($cleanPDO->commit());
         $stmt->execute();
         $res = $stmt->fetch();
         $this->assertTrue($res['value'] == 1);
