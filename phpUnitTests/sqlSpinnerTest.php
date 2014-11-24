4f1ecb9a919562552ebd214df01da8e3a3604d17
Steven Chennault <schenn@gmail.com>
2014-11-24 08:43:22 -0800

    Some skeleton for sqlSpinnerTest

--- phpUnitTests/sqlSpinnerTest.php
+++ phpUnitTests/sqlSpinnerTest.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Schenn
- * Date: 11/23/2014
- * Time: 11:21 PM
- */ 
+include_once("../PDOI/Utils/sqlSpinner.php");
+use PDOI\Utils\sqlSpinner as sqlSpinner;
+
+class sqlSpinnerTest extends PHPUnit_Framework_TestCase{
+
+
+}
